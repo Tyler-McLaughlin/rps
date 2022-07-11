4dbacd41a5f492af21de75b6bf11cf48f96919e2
Tyler-McLaughlin <tmclaughlin791@gmail.com>
2022-07-11 16:51:28 -0500

    Added a 'game' function which plays five games of rock, paper scissors and displays who won the game.

--- script.js
+++ script.js
@@ -3,25 +3,51 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection == computerSelection) {
-    return `You both selected ${computerSelection}! The round is a tie!`;
-  } else if (playerSelection == "rock") {
-    if (computerSelection == "paper") {
-      return "Paper covers rock. You lose!";
-    } else return "Rock smashes scissors. You win!";
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "rock") {
-      return "Paper covers rock. You win!";
-    } else return "Scissors cuts paper. You lose!";
+  if (playerSelection === computerSelection) {
+    return console.log(
+      `You both selected ${computerSelection}! The round is a tie!`
+    );
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      compWin++;
+      return console.log("Paper covers rock. You lose!");
+    } else playerWin++;
+    return console.log("Rock smashes scissors. You win!");
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      playerWin++;
+      return console.log("Paper covers rock. You win!");
+    } else compWin++;
+    return console.log("Scissors cuts paper. You lose!");
   } else {
-    if (computerSelection == "rock") {
-      return "Rock smashes scissors. You lose!";
-    } else return "Scissors cuts paper. You win!";
+    if (computerSelection === "rock") {
+      compWin++;
+      return console.log("Rock smashes scissors. You lose!");
+    } else playerWin++;
+    return console.log("Scissors cuts paper. You win!");
   }
 }
 
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(
+      "Choose rock, paper or scissors",
+      "Rock"
+    ).toLowerCase();
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+  }
+  if (playerWin === compWin) {
+    return console.log(
+      `The game is a tie! The score ends at ${playerWin} to ${compWin}`
+    );
+  } else if (playerWin > compWin) {
+    return console.log(`You win the game ${playerWin} to ${compWin}!`);
+  }
+  return console.log(`You lose the game ${compWin} to ${playerWin}!`);
+}
+
 const weapons = ["rock", "paper", "scissors"];
-const playerSelection = "SCISSORS";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+let compWin = 0;
+let playerWin = 0;
+console.log(game());
