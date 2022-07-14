e2f0c29254146082320d4c57a6d45a057a777c85
Tyler-McLaughlin <tmclaughlin791@gmail.com>
2022-07-14 13:38:08 -0500

    Created event listener to choose selection of weapon. I can't seem to get it to only choose the button elements. When I click the page, it displays a bunch of information I don't care to see.

--- script.js
+++ script.js
@@ -28,26 +28,40 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt(
-      "Choose rock, paper or scissors",
-      "Rock"
-    ).toLowerCase();
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-  }
-  if (playerWin === compWin) {
-    return console.log(
-      `The game is a tie! The score ends at ${playerWin} to ${compWin}`
-    );
-  } else if (playerWin > compWin) {
-    return console.log(`You win the game ${playerWin} to ${compWin}!`);
-  }
-  return console.log(`You lose the game ${compWin} to ${playerWin}!`);
-}
+window.addEventListener("click", function (e) {
+  const container = document.querySelector(".container");
+  const display = document.createElement("div");
+
+  playerSelection = e.target.textContent.toLowerCase();
+  computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+
+  display.classList.add("display");
+  display.textContent = `You chose ${playerSelection}! Computer chose ${computerSelection}!`;
+
+  container.appendChild(display);
+});
+
+// function game() {
+// for (let i = 0; i < 5; i++) {
+// let playerSelection = prompt(
+// "Choose rock, paper or scissors",
+// "Rock"
+// ).toLowerCase();
+// let computerSelection = computerPlay();
+// playRound(playerSelection, computerSelection);
+// }
+// if (playerWin === compWin) {
+// return console.log(
+// `The game is a tie! The score ends at ${playerWin} to ${compWin}`
+// );
+// } else if (playerWin > compWin) {
+// return console.log(`You win the game ${playerWin} to ${compWin}!`);
+// }
+// return console.log(`You lose the game ${compWin} to ${playerWin}!`);
+// }
 
 const weapons = ["rock", "paper", "scissors"];
 let compWin = 0;
 let playerWin = 0;
-console.log(game());
+// console.log(game());
