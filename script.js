0a5a794698c5a8e365704789c6b8b5f5bdfcc42a
Tyler-McLaughlin <tmclaughlin791@gmail.com>
2022-07-15 13:57:34 -0500

    Finished bare bones requirements of TOP to move on to the next section. Still needs a better looking UI. Can play a game of Rock, Paper Scissors until a player has won 5 rounds.

--- script.js
+++ script.js
@@ -1,78 +1,108 @@
+const weapons = ["rock", "paper", "scissors"];
+let compWin = 0;
+let playerWin = 0;
+
 function computerPlay() {
   return weapons[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerWin === 5) {
-    return `You win! Player - ${playerWin} || Computer - ${compWin}`;
-  } else if (compWin === 5) {
-    return `Computer wins! Player - ${playerWin} || Computer - ${compWin}`;
+  while (playerWin < 5 && compWin < 5) {
+    if (playerSelection === computerSelection) {
+      return `You both selected ${computerSelection}! The round is a tie!`;
+    } else if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        compWin++;
+        return `Paper covers rock. You lose!`;
+      } else playerWin++;
+      return `Rock smashes scissors. You win!`;
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        playerWin++;
+        return `Paper covers rock. You win!`;
+      } else compWin++;
+      return `Scissors cuts paper. You lose!`;
+    } else {
+      if (computerSelection === "rock") {
+        compWin++;
+        return `Rock smashes scissors. You lose!`;
+      } else playerWin++;
+      return `Scissors cuts paper. You win!`;
+    }
   }
-  if (playerSelection === computerSelection) {
-    return `You both selected ${computerSelection}! The round is a tie! The score: Player - ${playerWin} || Computer - ${compWin}`;
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      compWin++;
-      return `Paper covers rock. You lose! The score: Player - ${playerWin} || Computer - ${compWin}`;
-    } else playerWin++;
-    return `Rock smashes scissors. You win! The score: Player - ${playerWin} || Computer - ${compWin}`;
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      playerWin++;
-      return `Paper covers rock. You win! The score: Player - ${playerWin} || Computer - ${compWin}`;
-    } else compWin++;
-    return `Scissors cuts paper. You lose! The score: Player - ${playerWin} || Computer - ${compWin}`;
-  } else {
-    if (computerSelection === "rock") {
-      compWin++;
-      return `Rock smashes scissors. You lose! The score: Player - ${playerWin} === Computer - ${compWin}`;
-    } else playerWin++;
-    return `Scissors cuts paper. You win! The score: Player - ${playerWin} || Computer - ${compWin}`;
+  if (playerWin === 5) {
+    return `YOU BEAT THE COMPUTER! ${playerWin} to ${compWin}!`;
+  } else
+    return `THE COMPUTER DOMINATES AS EXPECTED! ${compWin} to ${playerWin}!`;
+}
+
+function checkWinner(playerWin, compWin) {
+  if (playerWin === 5) {
+    display.textContent = `YOU BEAT THE COMPUTER! ${playerWin} to ${compWin}!`;
+  } else if (compWin === 5) {
+    display.textContent = `THE COMPUTER DOMINATES AS EXPECTED! ${compWin} to ${playerWin}!`;
   }
 }
+
+function playAgain(playerWin, compWin) {
+  if (playerWin === 5 || compWin === 5) {
+    winner.textContent = "Play again?";
+    container.appendChild(winner);
+  }
+}
+
+const container = document.querySelector(".container");
+const display = document.createElement("div");
+display.classList.add("display");
+const winner = document.createElement("div");
+winner.classList.add("display");
+container.appendChild(display);
+
+const playerContainer = document.querySelector(".player-container");
+const playerScore = document.createElement("div");
+playerScore.textContent = playerWin;
+playerScore.classList.add("display");
+playerContainer.appendChild(playerScore);
+
+const compContainer = document.querySelector(".comp-container");
+const compScore = document.createElement("div");
+compScore.textContent = compWin;
+compScore.classList.add("display");
+compContainer.appendChild(compScore);
 
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
 rock.addEventListener("click", () => {
-  const container = document.querySelector(".container");
-  const display = document.createElement("div");
-
   playerSelection = rock.textContent.toLowerCase();
   computerSelection = computerPlay();
   display.textContent = playRound(playerSelection, computerSelection);
-  display.classList.add("display");
-
-  container.appendChild(display);
+  compScore.textContent = compWin;
+  playerScore.textContent = playerWin;
+  checkWinner(playerWin, compWin);
+  playAgain();
 });
 
 paper.addEventListener("click", () => {
-  const container = document.querySelector(".container");
-  const display = document.createElement("div");
-
   playerSelection = paper.textContent.toLowerCase();
   computerSelection = computerPlay();
   display.textContent = playRound(playerSelection, computerSelection);
-
-  display.classList.add("display");
-
-  container.appendChild(display);
+  compScore.textContent = compWin;
+  playerScore.textContent = playerWin;
+  checkWinner(playerWin, compWin);
+  playAgain();
 });
 
 scissors.addEventListener("click", () => {
-  const container = document.querySelector(".container");
-  const display = document.createElement("div");
-
   playerSelection = scissors.textContent.toLowerCase();
   computerSelection = computerPlay();
   display.textContent = playRound(playerSelection, computerSelection);
-
-  display.classList.add("display");
-
-  container.appendChild(display);
+  compScore.textContent = compWin;
+  playerScore.textContent = playerWin;
+  checkWinner(playerWin, compWin);
+  playAgain;
 });
-
 // function game() {
 // for (let i = 0; i < 5; i++) {
 // let playerSelection = prompt(
@@ -91,8 +121,3 @@ scissors.addEventListener("click", () => {
 // }
 // return console.log(`You lose the game ${compWin} to ${playerWin}!`);
 // }
-
-const weapons = ["rock", "paper", "scissors"];
-let compWin = 0;
-let playerWin = 0;
-// console.log(game());
